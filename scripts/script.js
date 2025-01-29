0ca471b7f3896e99c39303df5c9e26e607c1aa36
Damian Swarts <damianswarts2019@gmail.com>
2025-01-29 16:47:14 +0200

    Added an empty array to store the books, a constructor for the books and an empty function that can create a book and store the book into the array

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+
+    this.info = function() {
+        return this.title + " by " + this.author + ", " + this.pages + ", " + this.read;
+    }    
+}
+
+
+function addBookToLibrary() {
+  // take params, create a book then store it in the array
+}
