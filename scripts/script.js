3264a6c4a9ebc0a2e2a019b7b151165079a2db74
Damian Swarts <damianswarts2019@gmail.com>
2025-02-10 20:48:48 +0200

    Added a function that loops through the myLibrary[] array and displays each book object on a card on the page. Also added some basic styling to the page.

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,54 @@
+// Get the container element
+var container = document.getElementById('container');
+
+// Empty array in which all book objects will be stored
 const myLibrary = [];
 
+// Constructor function to create book objects
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read;
-
-    this.info = function() {
-        return this.title + " by " + this.author + ", " + this.pages + ", " + this.read;
-    }    
-}
-
+    this.read = read; 
+};
 
 function addBookToLibrary(title, author, pages, read) {
-  // take params, create a book then store it in the array
+  // Take params, create a book then store it in the array
     const book = new Book(title, author, pages, read);
-	myLibrary.push(book);
-}
+	  myLibrary.push(book);
+};
+
+// Function that loops through the array 
+// and displays each book in a card on the page
+function displayBooks(library) {
+  library.forEach(book => {
+      // Create card elements
+      let card = document.createElement('div');
+      card.classList.add('card');
+      card.classList.add(book.title);
+
+      let title = document.createElement('h2');
+      title.textContent = book.title;
+
+      let author = document.createElement('p');
+      author.textContent = `Author: ${book.author}`;
+
+      let pages = document.createElement('p');
+      pages.textContent = `Pages: ${book.pages}`;
+
+      let read = document.createElement('p');
+      read.textContent = `Status: ${book.read}`;
+
+      // Append elements to the card
+      card.appendChild(title);
+      card.appendChild(author);
+      card.appendChild(pages);
+      card.appendChild(read);
+
+      // Append card to the container
+      container.appendChild(card);
+  });
+};
+
+// Call the function to display books
+displayBooks(myLibrary);
